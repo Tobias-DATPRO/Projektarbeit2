c273dd90971c60e8e5603646658d45f44eb5166b
Tobias-DATPRO <twurzel@smail.uni-koeln.de>
2024-12-28 16:48:00 +0100

    Schritt 1
    Erste Module
    Anlegen der Simulationsdatei

---
+++ Konstanten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Werte der benötigten Konstanten:
+
+#Masse eines Teilchens
+m = 1.0
+#Ladung eines Teilchens
+q = 50.0
+#Gravitationskonstante
+g = -10.0
+
